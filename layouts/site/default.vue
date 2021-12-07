bf333ba54509fe644a2c49df85afc958f355cda4
PAULO CESAR <paulo.pcbr@gmail.com>
2021-12-07 18:36:29 -0300

    feat: :sparkles: Criar rota ahub.tech/discord
    Apontar para: https://discord.com/invite/RMDSHVkDmE
    
    #3

--- layouts/site/default.vue
+++ layouts/site/default.vue
@@ -1,174 +1,222 @@
 <template>
+  <div>
+    <header>
+      <div class="header_container">
+        <img src="logo.png" alt="HUB" />
 
-    <div>
+        <template v-for="(item, index) in menus">
+          <nuxt-link
+            :key="`header_link-${item.to}-nuxt`"
+            v-if="item.type === 'page'"
+            class="header_link"
+            :to="item.to"
+          >
+            {{ item.label }}
+          </nuxt-link>
+          <a
+            v-else
+            :key="`header_link-${item.to}-a`"
+            class="header_link"
+            :href="item.to"
+            :target="item.target || '_blank'"
+          >
+            {{ item.label }}
+          </a>
+        </template>
 
-        <header>
-            <div class="header_container">
-                <img src="logo.png" alt="">
-                <nuxt-link class="header_link" to="/">INICIO</nuxt-link>
-                <a class="header_link" href="https://discord.com/invite/RMDSHVkDmE" target="_blank">QUERO PARTICIPAR</a>
-                <a class="header_link" href="https://blog.ahub.tech/" target="_blank">BLOG</a>
-                <i @click="isMenuOpen = !isMenuOpen" class="mdi mdi-menu open_mobile_menu"></i>
-            </div>
-        </header>
+        <i
+          @click="isMenuOpen = !isMenuOpen"
+          class="mdi mdi-menu open_mobile_menu"
+        ></i>
+      </div>
+    </header>
 
-        <div class="mobile_menu" v-if="isMenuOpen">
-            <div class="header_mobile_menu">
-                <i @click="isMenuOpen = !isMenuOpen" class="mdi mdi-close close_menu"></i>
-            </div>
-            <nuxt-link class="header_link" to="/">INICIO</nuxt-link>
-            <a class="header_link" href="https://discord.com/invite/RMDSHVkDmE" target="_blank">QUERO PARTICIPAR</a>
-            <a class="header_link" href="https://blog.ahub.tech/" target="_blank">BLOG</a>
-        </div>
-
-        <Nuxt />
+    <div class="mobile_menu" v-if="isMenuOpen">
+      <div class="header_mobile_menu">
+        <i
+          @click="isMenuOpen = !isMenuOpen"
+          class="mdi mdi-close close_menu"
+        ></i>
+      </div>
+      <template v-for="(item, index) in menus">
+        <nuxt-link
+          :key="`header_link-${item.to}-nuxt-mobile`"
+          v-if="item.type === 'page'"
+          class="header_link"
+          :to="item.to"
+        >
+          {{ item.label }}
+        </nuxt-link>
+        <a
+          v-else
+          :key="`header_link-${item.to}-a-mobile`"
+          class="header_link"
+          :href="item.to"
+          :target="item.target || '_blank'"
+        >
+          {{ item.label }}
+        </a>
+      </template>
     </div>
 
+    <Nuxt />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'default',
-        data() {
-            return {
-                isMenuOpen: false
-            }
-        }
-    }
+export default {
+  name: "default",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  computed: {
+    menus() {
+      // type [page] - usa nuxt-link
+      // type [open_link] - usa tag A href
+      return [
+        { type: "page", to: "/", label: "INICIO" },
+        {
+          type: "open_link",
+          to: "/discord",
+          label: "QUERO PARTICIPAR",
+          target: "_blank",
+        },
+        {
+          type: "open_link",
+          to: "https://blog.ahub.tech/",
+          label: "BLOG",
+          target: "_blank",
+        },
+      ];
+    },
+  },
+};
 </script>
 
-
 <style scoped>
+header {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-bottom: solid 1px #3e3d3d;
+}
 
-    header {
-        width: 100%;
-        height: 70px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        top: 0;
-        left: 0;
-        border-bottom: solid 1px #3E3D3D;
-    }
+header .header_container {
+  width: 1300px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  column-gap: 30px;
+}
 
-    header .header_container {
-        width: 1300px;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        column-gap: 30px;
-    }
+header .header_container img {
+  width: 80px;
+}
 
-    header .header_container img {
-        width: 80px;
-    }
+header .header_container .header_link {
+  font-size: 12px;
+  font-weight: 500;
+  letter-spacing: 2px;
+  color: white;
+  transition: 0.5s;
+}
 
-    header .header_container .header_link {
-        font-size: 12px;
-        font-weight: 500;
-        letter-spacing: 2px;
-        color: white;
-        transition: .5s;
-    }
+header .header_container .header_link:hover {
+  color: #f73b91;
+}
 
-    header .header_container .header_link:hover {
-        color: #F73B91;
-    }
+header .header_container .open_mobile_menu {
+  display: none;
+}
 
-    header .header_container .open_mobile_menu {
-        display: none;
-    }
+.mobile_menu {
+  width: 75%;
+  background: #19191c;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 102;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  animation: fromRight 0.5s ease-in-out;
+}
 
-    .mobile_menu {
-        width: 75%;
-        background: #19191C;
-        height: 100vh;
-        position: fixed;
-        top: 0;
-        right: 0;
-        z-index: 102;
-        padding: 20px;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        animation: fromRight .5s ease-in-out;
-    }
+.mobile_menu .header_mobile_menu {
+  display: flex;
+  margin-bottom: 20px;
+}
 
-    .mobile_menu .header_mobile_menu {
-        display: flex;
-        margin-bottom: 20px;
-    }
+.mobile_menu .header_mobile_menu i {
+  display: flex;
+  font-size: 25px;
+  color: white;
+}
 
-    .mobile_menu .header_mobile_menu i {
-        display: flex;
-        font-size: 25px;
-        color: white;
-    }
+.mobile_menu .header_link {
+  font-size: 12px;
+  font-weight: 500;
+  letter-spacing: 2px;
+  color: white;
+  transition: 0.5s;
+  margin-bottom: 10px;
+}
 
-    .mobile_menu .header_link {
-        font-size: 12px;
-        font-weight: 500;
-        letter-spacing: 2px;
-        color: white;
-        transition: .5s;
-        margin-bottom: 10px;
-    }
+/* keyframes */
 
-    /* keyframes */
+@keyframes fromRight {
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
 
-    @keyframes fromRight {
-        0% {
-            opacity: 0;
-            transform: translateX(100%);
-        }
-        100% {
-            opacity: 1;
-            transform: translateX(0);
-        }
-    }
+/* responsive */
 
-    /* responsive */
+@media (max-width: 1320px) {
+  header .header_container {
+    width: 900px;
+  }
+}
 
-    @media(max-width: 1320px) {
+@media (max-width: 920px) {
+  header .header_container {
+    width: 600px;
+  }
 
-        header .header_container {
-            width: 900px;
-        }
+  header .header_container img {
+    width: 50px;
+  }
+}
 
-    }
+@media (max-width: 620px) {
+  header .header_container {
+    width: 100%;
+    padding: 20px;
+    justify-content: space-between;
+  }
 
-    @media(max-width: 920px) {
+  header .header_container .header_link {
+    display: none;
+  }
 
-        header .header_container {
-            width: 600px;
-        }
-
-        header .header_container img {
-            width: 50px;
-        }
-        
-    }
-
-    @media(max-width: 620px) {
-
-        header .header_container {
-            width: 100%;
-            padding: 20px;
-            justify-content: space-between;
-        }
-
-        header .header_container .header_link {
-            display: none;
-        }
-
-        header .header_container .open_mobile_menu {
-            display: flex;
-            font-size: 25px;
-            color: white;
-        }
-
-    }
-
+  header .header_container .open_mobile_menu {
+    display: flex;
+    font-size: 25px;
+    color: white;
+  }
+}
 </style>
-
---
+++ pages/discord.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  middleware({ redirect, $config }) {
+    return redirect($config.discordLink);
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.location.href = this.$config.discordLink;
+    });
+  },
+};
+</script>
