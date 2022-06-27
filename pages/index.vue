9e5f707fdb937f0a5427a0bfc723d57daca6e1ae
daviprm-pc <daviprm@outlook.com>
2022-06-27 12:20:35 -0300

    feat: end pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,86 @@
 <template>
-  <main class="cover">
-    <div class="cover_container">
-      <div class="cover_text">
-        <h2>A HUB É <span>POP</span></h2>
-        <h2>A HUB É <span>TECH</span></h2>
-        <div class="title_bar"></div>
-        <p>
-          Aqui você encontrará os mais diversos assuntos pertinentes ao mundo de
-          TI, conhecerá diversas pessoas e se divertirá muito enquanto aprende e
-          ensina!
-        </p>
-        <a :href="discordLink" target="_blank" class="discord_btn">
-          <i class="mdi mdi-discord"></i>
-          QUERO FAZER PARTE
-        </a>
-      </div>
-      <img src="cover.svg" alt="" />
-    </div>
-    <div class="cover_links">
-      <a target="_blank" href="https://github.com/aHub-Tech" class="cover_link">
-        <i class="mdi mdi-github"></i>
-      </a>
-      <a target="_blank" :href="discordLink" class="cover_link">
-        <i class="mdi mdi-discord"></i>
-      </a>
-    </div>
-  </main>
+    
+    <main>
+        <img src="/logo.png" alt="">
+        <h2>Fim da Comunidade</h2>
+        
+
+        <p>Queridos Hubbles.</p>
+        <p>Em Maio de 2021 nasceu a HubTech, com o proposito de unir streamers e pessoas interessadas neste mundo Tech.</p>
+        <p>Sob os valores de união, colaboração, transparência e comprometimento, a Hub impactou diversas historias!</p>
+        <p>Porém, em Julho de 2022 notamos que a Hub nao vem sendo tão disruptiva como gostariamos, acabou então se tornando um grupo de amigos.</p>
+        <p>Como o objetivo da Hub acabou se perdendo, acreditamos que agora é um momento de cada um dos membros traçarem rumo à novos ares.</p>
+        <p>Criamos conexões, escrevemos histórias e com certeza marcamos o ciclo de muitas pessoas.</p>
+        <p>Gostaríamos de deixar aqui algumas comunidades que sabemos que tem muitos dos valores da Hub, e, que sempre nos apoiaram desde o inicio:</p>
+        <div class="items">
+            <a target="_blank" href="https://discord.gg/FP5UaAG">Collabcode</a>
+            <a target="_blank" href="https://discord.com/invite/5kwDQuv">He4rt Community</a>
+            <a target="_blank" href="https://discord.com/invite/gabicode">Gabi Code</a>
+            <a target="_blank" href="https://discord.com/invite/3UKZT7cVWG">O Bruno Germano</a>
+        </div>
+        <p>Desejamos à todos um futuro próspero e que possamos nos encontrar em outros ambientes.</p>
+        <p>De qualquer maneira fica o nosso obrigado por todos que compartilharam desse sonho e fizeram acontecer.</p>
+        <br>
+        <p>Att,</p>
+        <br>
+        <br>
+        <p>Time Staff 😊</p>
+
+
+    </main>
+
 </template>
 
-<script>
-export default {
-  layout: "site/default",
-  head() {
-    return {
-      title: "Inicio",
-    };
-  },
-  data() {
-    return {
-      discordLink: "/discord",
-    };
-  },
-};
-</script>
+<style scoped>
 
-<style src="~/assets/css/pages/Home.css" scoped></style>
+    main {
+        width: 700px;
+        padding: 40px;
+        background: #1b1b1d;
+        border-radius: 7px;
+        margin-top: 60px;
+        margin-bottom: 200px;
+    }
+
+    main img {
+        width: 100px;
+        margin-bottom: 20px;
+    }
+
+    main h2 {
+        color: rgb(255, 255, 255);
+        font-size: 18px;
+        font-weight: 400;
+        margin-bottom: 40px;
+    }
+
+    main p {
+        color: rgb(199, 199, 199);
+        font-size: 14px;
+        line-height: 1.5;
+        font-weight: 400;
+        margin-bottom: 10px;
+    }
+
+    main .items {
+        margin-top: 30px;
+        margin-bottom: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        row-gap: 10px;
+    }
+
+    main .items a {
+        color: #ffd235;
+        font-size: 14px;
+        font-weight: 400;
+        text-decoration: none;
+        transition: all .3s;
+    }
+
+    main .items a:hover {
+        text-decoration: underline;
+    }
+
+</style>
